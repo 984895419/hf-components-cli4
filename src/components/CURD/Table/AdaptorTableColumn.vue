b8fbb3a21fd8ef279a270a1c6e9f0afe36777c69
jiannansong <984895419@qq.com>
2022-09-05 14:52:57 +0800

    upd

---
+++ src/components/CURD/Table/AdaptorTableColumn.vue
@@ -0,0 +1,35 @@
+<template>
+  <component :is="tpName + '-table-column'" v-bind="$attrs" v-on="$listeners" />
+</template>
+<script>
+  import CreateTimeTableColumn from './column/CreateTimeTableColumn'
+  import CreatorTableColumn from './column/CreatorTableColumn'
+  import DefaultTableColumn from './column/DefaultTableColumn'
+  import EnableStateTableColumn from './column/EnableStateTableColumn'
+  import ImageTableColumn from './column/ImageTableColumn'
+  import ModifierTableColumn from './column/ModifierTableColumn'
+  import ModifyTimeTableColumn from './column/ModifyTimeTableColumn'
+  import DateTimeFormatTableColumn from './column/base/DateTimeFormatTableColumn'
+  import SectionTableColumn from './column/base/SectionTableColumn'
+  import TagTableColumn from './column/base/TagTableColumn'
+  export default {
+    components: {
+      CreateTimeTableColumn,
+      CreatorTableColumn,
+      DefaultTableColumn,
+      EnableStateTableColumn,
+      ImageTableColumn,
+      ModifierTableColumn,
+      ModifyTimeTableColumn,
+      DateTimeFormatTableColumn,
+      SectionTableColumn,
+      TagTableColumn
+    },
+    props: {
+      tpName: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
